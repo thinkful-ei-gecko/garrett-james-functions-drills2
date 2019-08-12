a5fbe05fc388eaf9f5d7a35f3a15cabd28cdc92e
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-12 15:58:45 -0400

    create and beyond function

--- drill2.js
+++ drill2.js
@@ -1,3 +1,15 @@
 function jediName(firstName, lastName){
     return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
+
+function beyond(num) {
+    if (num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY) {
+        return 'And Beyond!';
+    } else if (Number.isFinite(num) && num > 0) {
+        return 'To Infinity!';
+    } else if (Number.isFinite(num) && num < 0) {
+        return 'To Negative Infinity!';
+    } else if (num === 0) {
+        return 'Staying home...'
+    }
+}
